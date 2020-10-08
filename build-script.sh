caafc62f8a97dc530bc0ae67be51f5be5d6c77e9
Sibel <sibelclarus@gmail.com>
2020-10-08 00:44:24 -0400

    updated

--- build-script.sh
+++ build-script.sh
@@ -8,13 +8,13 @@ curl -L "https://github.com/docker/compose/releases/download/1.26.2/docker-compo
 -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 TOKEN="xxxxxxxxx"
-FOLDER="https://$TOKEN@raw.githubusercontent.com/clarusway/clarusway-aws-devops-1-20/master/devops/projects/202-dockerization-bookstore-api-on-python-flask-mysql/"
+FOLDER="https://$TOKEN@raw.githubusercontent.com/sibelozen/DevOpsProject202/main/"
 curl -s --create-dirs -o "/home/ec2-user/bookstore-api/bookstore-api.py" -L "$FOLDER"bookstore-api.py
 curl -s --create-dirs -o "/home/ec2-user/bookstore-api/requirements.txt" -L "$FOLDER"requirements.txt
 curl -s --create-dirs -o "/home/ec2-user/bookstore-api/Dockerfile" -L "$FOLDER"Dockerfile
 curl -s --create-dirs -o "/home/ec2-user/bookstore-api/docker-compose.yml" -L "$FOLDER"docker-compose.yml
 cd /home/ec2-user/bookstore-api
-docker build -t callahanclarus/bookstore-api:latest .
+docker build -t sibel/bookstore-api:latest .
 docker-compose up -d
 
 
